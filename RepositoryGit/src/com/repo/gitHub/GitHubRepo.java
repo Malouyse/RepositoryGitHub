2af7278611c07dcefc94c00ab1312a200d5d4b5b
Judith Aio <Judith Aio@DESKTOP-KSD455U>
2019-04-02 16:15:03 -0400

    1st code Bazir

--- RepositoryGit/src/com/repo/gitHub/GitHubRepo.java
+++ RepositoryGit/src/com/repo/gitHub/GitHubRepo.java
@@ -1,5 +1,37 @@
 package com.repo.gitHub;
 
-public class GitHubRepo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GitHubRepo {
+	
+	
+	
+	public static void main(String[] args) {
+
+
+        List<String> verticesposition2 = new ArrayList<String>();
+
+        verticesposition2.add("safdsfVertex");
+
+        verticesposition2.add("safdsfNamesfsd");
+     verticesposition2.add("notCONTAINS");
+        verticesposition2.add(" shoes");
+
+        for (String str5 : verticesposition2){
+
+            if((str5.contains("Vertex")||str5.contains("Name")||str5.contains("Transmittance")||str5.contains("shoes"))){
+
+                System.out.println(str5);
+                
+                String git = "hello, I am new to gitHub";
+
+       
+            }
+        }
+	
+	}
+	
+
+	
 }
